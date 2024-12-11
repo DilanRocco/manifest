d907307b1e6c4671a7309f051ac27e00b0ec82ab
DilanRocco <captaindilan12@gmail.com>
2024-12-11 17:11:30 -0500

    Remove logo to improve load times

--- src/UI/signup.tsx
+++ src/UI/signup.tsx
@@ -107,7 +107,7 @@ const SignupForm = () => {
 
   return (
     <VStack minW="20rem" >
-      <Image src={ManifestaLogo} alt="Manifesta Logo"/>
+      <Heading size="4xl">Manifest</Heading>
       <Spacer />
       <HStack w='100%'>
         <Field label="First Name"><Input name={"first"} value={formData.first} onChange={handleChange} placeholder="John"/></Field>
